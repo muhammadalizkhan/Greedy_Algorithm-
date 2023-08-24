0dc0083db686731efd94d135896049b42a3c8c1a
Muhamad Ali Khan <72930799+muhammadalizkhan@users.noreply.github.com>
2023-08-24 21:02:40 +0500

    Create NS_Array_ObjectC.m

---
+++ NS_Array_ObjectC.m
@@ -0,0 +1,39 @@
+#import <Foundation/Foundation.h>
+
+NSArray* greedyCoinChange(NSArray* availableCoins, NSInteger targetAmount) {
+    NSMutableArray* result = [NSMutableArray array];
+    NSInteger remainingAmount = targetAmount;
+    
+    // Sort coins in descending order
+    NSArray* sortedCoins = [availableCoins sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
+        return [b integerValue] - [a integerValue];
+    }];
+    
+    for (NSNumber* coin in sortedCoins) {
+        NSInteger coinValue = [coin integerValue];
+        while (remainingAmount >= coinValue) {
+            [result addObject:coin];
+            remainingAmount -= coinValue;
+        }
+    }
+    
+    if (remainingAmount == 0) {
+        return result;
+    } else {
+        NSLog(@"Cannot make exact change.");
+        return nil;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSArray* availableCoins = @[@25, @10, @5, @1]; // Available coin denominations
+        NSInteger targetAmount = 63; // Target amount to make change for
+        
+        NSArray* change = greedyCoinChange(availableCoins, targetAmount);
+        if (change) {
+            NSLog(@"Greedy coin change: %@", change);
+        }
+    }
+    return 0;
+}
